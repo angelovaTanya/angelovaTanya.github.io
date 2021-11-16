4814fab186882123bb86701fbc1f1d0b63d0d175
Tanya Angelova <tanya0703@abv.bg>
2021-11-16 17:33:02 +0200

    education

--- js/script.js
+++ js/script.js
@@ -1,45 +1,50 @@
 // active menu on click in the navigation bar
 $("#navAbout").click(function() {
         $(".navigation ul li").children().removeClass("nav-active");
+        $(".navigation ul li").children().removeClass("activeOnScroll");
         $(this).addClass("nav-active");
            $('html, body').animate({
-               scrollTop:        $("#about").offset().top-66
+               scrollTop:        $("#about").offset().top
            }, 500);
         return false;
        });
 
       $("#navSkills").click(function() {
         $(".navigation ul li").children().removeClass("nav-active");
+        $(".navigation ul li").children().removeClass("activeOnScroll");
         $(this).addClass("nav-active");
            $('html, body') .animate({
-               scrollTop:        $("#skills").offset().top-112
+               scrollTop:        $("#skills").offset().top
            }, 500);
         return false;
        });
 
       $("#navExp").click(function() {
         $(".navigation ul li").children().removeClass("nav-active");
+        $(".navigation ul li").children().removeClass("activeOnScroll");
         $(this).addClass("nav-active");
            $(' html,body') .animate({
-               scrollTop:        $("#experience").offset().top-112
+               scrollTop:        $("#experience").offset().top
            }, 1000);
         return false;
        });
 
       $("#navEdu").click(function() {
         $(".navigation ul li").children().removeClass("nav-active");
+        $(".navigation ul li").children().removeClass("activeOnScroll");
         $(this).addClass("nav-active");
            $('html,body ') .animate({
-               scrollTop:        $("#education").offset().top-112
+               scrollTop:        $("#education").offset().top
            }, 1000);
         return false;
        });
 
        $("#navContact").click(function() {
          $(".navigation ul li").children().removeClass("nav-active");
+         $(".navigation ul li").children().removeClass("activeOnScroll");
          $(this).addClass("nav-active");
             $('html,body ') .animate({
-                scrollTop:        $("#contact").offset().top-112
+                scrollTop:        $("#contact").offset().top
             }, 1000);
          return false;
         });
@@ -51,7 +56,7 @@ const infosTextAppear = document.querySelectorAll('.skill-bars .bar .info span')
 const sliders = document.querySelectorAll('.slide-in, .slide-in-left');
 
 const appearOptions = {
-  threshold: 0,
+  threshold: 0.1,
   rootMargin: "0px 0px -250px 0px"
 }
 
@@ -91,12 +96,13 @@ function onScroll(event){
     $('.nav-link').each(function () {
         var currLink = $(this);
         var refElement = $(currLink.attr("href"));
-        if (refElement.position().top - 111 <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+        if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
+            $('.nav-link').removeClass("activeOnScroll"); //added to remove active class from all a elements
             $('.nav-link').removeClass("nav-active"); //added to remove active class from all a elements
-            currLink.addClass("nav-active");
+            currLink.addClass("activeOnScroll");
         }
         else{
-            currLink.removeClass("nav-active");
+            currLink.removeClass("activeOnScroll");
         }
     });
 }
